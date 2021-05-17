2944f325e4f65f8b7408c708b605b7d227cadd3c
Carson Burke <carsonburke22@gmail.com>
2021-05-17 14:00:27 -0700

    parallax now operates using percentages w/ bounding rect

--- parallaxify.js
+++ parallaxify.js
@@ -28,14 +28,15 @@ function parallax() {
 
         function getPosition(item) {
 
-            let top = item.getBoundingClientRect().top
+            let top = (item.getBoundingClientRect().top / window.innerHeight * 100).toFixed(0)
 
             return top
         }
+        console.log(getPosition(item))
 
         if (!item.dataset.speedMultiplier) {
 
-            item.dataset.speedMultiplier = "0.7"
+            item.dataset.speedMultiplier = "0.5"
         }
         if (/Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
 
@@ -43,7 +44,7 @@ function parallax() {
         } else {
 
             item.style.backgroundAttachment = "none"
-            item.style.backgroundPositionY = getPosition(item) * -1 * item.dataset.speedMultiplier + "px"
+            item.style.backgroundPositionY = getPosition(item) * item.dataset.speedMultiplier + "%"
         }
     }
 }
